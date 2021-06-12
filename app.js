35d0e55ee8254e5fd59f1fc32395b47a5497eade
Chris Santos <christinasantos2413@gmail.com>
2021-06-12 13:57:33 -0400

    updated content on both .html files to be consistent while navigating across pages

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log('my js file has STARTED...');
+
+var myNumber = 200;
+
+function addition(number1, number2) {
+    // whatever operation.
+    return number1 + number2;
+
+}
+
+var myArray = ['hello', 'world', 200];
